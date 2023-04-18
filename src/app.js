0a89850dda2f4de0fa58821c0be2a3dd4c65f1d1
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-18 15:51:52 -0300

    SignUp page implemented

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
 import { MongoClient } from "mongodb";
-import { Express } from "express";
+import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import Joi from "joi";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try {
+  await mongoClient.connect();
+} catch (err) {
+  console.log(err.message);
+}
+const db = mongoClient.db();
+
+server.post("/users", async (req, res) => {
+  const userSchema = Joi.object({
+    nome: Joi.string().required(),
+    email: Joi.string().email().required(),
+    senha: Joi.string().min(3).required(),
+  });
+  if (userSchema.validate(req.body).error) {
+    return res.sendStatus(422);
+  }
+  try {
+    const usuario = await db
+      .collection("users")
+      .findOne({ email: req.body.email });
+    if (usuario) {
+      return res.sendStatus(409);
+    }
+    await db.collection("users").insertOne(req.body);
+    return res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+const PORT = 5001;
+server.listen(PORT, console.log(`Server running on port ${PORT}`));
