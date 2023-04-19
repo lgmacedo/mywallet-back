cfe45536516959a47fdabfc6d8bb07fe167615bd
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-19 13:47:52 -0300

    SignIn page implemented

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import Joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 const server = express();
 server.use(cors());
@@ -17,15 +19,18 @@ try {
 }
 const db = mongoClient.db();
 
-server.post("/users", async (req, res) => {
+server.post("/sign-up", async (req, res) => {
+  const { nome, email, senha } = req.body;
   const userSchema = Joi.object({
     nome: Joi.string().required(),
     email: Joi.string().email().required(),
     senha: Joi.string().min(3).required(),
   });
-  if (userSchema.validate(req.body).error) {
-    return res.sendStatus(422);
+  const validation = userSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    return res.status(422).send(validation.error.details.map((d) => d.message));
   }
+  const passwordHash = bcrypt.hashSync(senha, 10);
   try {
     const usuario = await db
       .collection("users")
@@ -33,10 +38,36 @@ server.post("/users", async (req, res) => {
     if (usuario) {
       return res.sendStatus(409);
     }
-    await db.collection("users").insertOne(req.body);
+    await db
+      .collection("users")
+      .insertOne({ nome, email, senha: passwordHash });
     return res.sendStatus(201);
   } catch (err) {
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
+  }
+});
+
+server.post("/sign-in", async (req, res) => {
+  const { email, senha } = req.body;
+  const userSchema = Joi.object({
+    email: Joi.string().email().required(),
+    senha: Joi.string().min(3).required(),
+  });
+  const validation = userSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    return res.status(422).send(validation.error.details.map((d) => d.message));
+  }
+  try {
+    const user = await db.collection("users").findOne({ email: email });
+    if (!user) return res.status(404).send("Email não cadastrado");
+    console.log(user);
+    if (!bcrypt.compareSync(senha, user.senha))
+      return res.status(401).send("Senha não confere");
+    const token = uuid();
+    await db.collection("sessions").insertOne({ userId: user._id, token });
+    return res.status(200).send(token);
+  } catch (err) {
+    return res.status(500).send("Erro inesperado. Tente novamente.");
   }
 });
 
